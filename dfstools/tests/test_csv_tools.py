3a094e58bfdd5e8f95b14fc1abe49e31daba5458
mclaes <mclaes@cisco.com>
2020-07-14 14:00:38 -0400

    Added test_find_csv_files_include_hidden

--- dfstools/tests/test_csv_tools.py
+++ dfstools/tests/test_csv_tools.py
@@ -4,6 +4,7 @@ import git
 import os
 from dfstools import show_aggie_pride
 from dfstools import load_csv_to_df
+from dfstools import find_csv_files
 
 
 class CsvTools(unittest.TestCase):
@@ -19,6 +20,45 @@ class CsvTools(unittest.TestCase):
 
         # make sure the message was actually printed 
         self.assertEqual('Aggie Pride - Worldwide\n', out)
+
+    def test_find_csv_files_include_hidden(self):
+        # This will test to make sure include_hidden parameter works as expected
+        # Find the data directory for this repo
+        data_path = os.path.join(git.Repo('.', search_parent_directories=True).working_tree_dir, 'data')
+
+        # Test the case where include_hidden==False
+        # List of filenames we expect to see.  Order is not important
+        # Full path is not important
+        expected = {'flights.csv',
+                    'invalid_logs.csv',
+                    'trip_logs.csv',
+                    'airlines.csv',
+                    'airports.csv'}
+
+        result = find_csv_files(data_path,
+                                include_hidden=False,
+                                traverse_subdir=True,
+                                follow_symlink=False)
+
+        # Make sure all expected file names are present
+        for e in expected:
+            self.assertIn(e, next((s for s in result if e in s), result))
+
+        # .old_trip_logs.csv should not be present since it is a hidden file
+        self.assertNotIn('.old_trip_logs.csv', result)
+
+        # Now test the true case
+        result = find_csv_files(data_path,
+                                include_hidden=True,
+                                traverse_subdir=True,
+                                follow_symlink=False)
+
+        # Make sure all expected file names are present
+        for e in expected:
+            self.assertIn(e, next((s for s in result if e in s), result))
+
+        # .old_trip_logs.csv should now be present
+        self.assertIn('.old_trip_logs.csv', result)
 
     def test_load_csv_to_df(self):
         expected = {'airlines', 'airports', 'flights', 'trip_logs'}
